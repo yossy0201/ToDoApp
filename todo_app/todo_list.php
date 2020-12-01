ecbe19f0c5c16d6835ea9e314039344b2e36dd3d
yossy0201 <yossy0201msk@gmail.com>
2020-12-01 22:09:31 +0900

    todo-list

---
+++ todo_app/todo_list.php
@@ -0,0 +1,11 @@
+<?php
+require_once("function.php");
+
+$todo_list = read_todo_list(false);
+
+
+for ($i = 0; $i < count($todo_list); $i++) {
+  $todo = $todo_list[$i];
+  echo $todo[0] . ":" . $todo[1] . "(" . $todo[2] .")"
+  . PHP_EOL;
+}
